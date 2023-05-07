2386d0e74a8ab3873f977c41686cd66c5dabbb53
IlanVaks <ilanvaks@gmail.com>
2023-05-07 12:26:49 -0400

    cleanedUp-addedHome

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './scenes/Home';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home/>
       </header>
     </div>
   );
