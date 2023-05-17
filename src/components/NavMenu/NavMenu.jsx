20e78aed13e0388056376b0b7403f3a60b8525bc
IlanVaks <ilanvaks@gmail.com>
2023-05-17 01:47:29 -0400

    fixed-nav

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,8 +1,6 @@
-import { Container, Nav, Navbar, NavDropdown,} from "react-bootstrap";
-import SplitButton from "react-bootstrap/SplitButton";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { signOut } from "firebase/auth";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import "../../styles/NavMenu.css";
@@ -18,9 +16,7 @@ export default function NavMenu({ user, setUser }) {
   };
 
   const navigate = useNavigate();
-
   const app = initializeApp(firebaseConfig);
-
   const auth = getAuth(app);
 
   const handleLogout = () => {
@@ -37,58 +33,35 @@ export default function NavMenu({ user, setUser }) {
   return (
     <Navbar className="custom-navbar" expand="lg">
       <Container fluid>
-        <div className="brand-container">
-          <Navbar.Brand href="/">Home</Navbar.Brand>
-        </div>       
-          <Nav>
-            <div className="aal-container">
-              <Nav.Item>
-                <div className="AboutMe-container">
-                  <Nav.Link as={Link} to="/AboutMe" className="nav-link">
-                    About Me
-                  </Nav.Link>
-                </div>
-              </Nav.Item>
-              <Nav.Link id="basic-nav-dropdown">
-                {!user ? 
-                  <Nav.Link as={Link} to="/Login">
-                    Add Sneaker
-                  </Nav.Link>
-                 : 
-                  <Nav.Link as={Link} to="/AddSneaker">
-                    Add Sneaker
-                  </Nav.Link>
-                }
+        <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/AboutMe">
+            About Me
+          </Nav.Link>
+          {!user ? (
+            <Nav.Link as={Link} to="/Login">
+              Add Sneaker
+            </Nav.Link>
+          ) : (
+            <Nav.Link as={Link} to="/AddSneaker">
+              Add Sneaker
+            </Nav.Link>
+          )}
+          {!user ? (
+            <Nav.Link as={Link} to="/Login">
+              Login
+            </Nav.Link>
+          ) : (
+            <>
+              <Nav.Link style={{ pointerEvents: "none" }}>
+                {user.displayName}
               </Nav.Link>
-              {!user ? 
-                <Nav.Item>
-                  <div className="Login-Container">
-                    <Nav.Link as={Link} to="/Login" className="nav-link">
-                      Login
-                    </Nav.Link>
-                  </div>
-                </Nav.Item>
-               : 
-                <>
-                  <Nav.Item>
-                    <div className="diplayname-container">
-                      {user.displayName}
-                    </div>
-                  </Nav.Item>
-                  <Nav.Link as={Link} onClick={handleLogout}>
-                    Logout
-                  </Nav.Link>
-                </>
-              }
-            </div>
-          </Nav>
-       
-        {/* <img
-          src="../../../images/shoe360.gif"
-          width="180"
-          height="180"
-          alt="Brand logo"
-        /> */}
+              <Nav.Link as={Link} onClick={handleLogout}>
+                Logout
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
       </Container>
     </Navbar>
   );
