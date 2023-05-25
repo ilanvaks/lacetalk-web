b5a2a74bb3a51dde455950aaa487cc7408e19a9f
IlanVaks <ilanvaks@gmail.com>
2023-05-25 17:56:10 -0400

    added-login-toVote

--- src/scenes/UpdateVote.jsx
+++ src/scenes/UpdateVote.jsx
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { IoMdThumbsUp as ThumbsUp, IoMdThumbsDown as ThumbsDown } from 'react-icons/io'
 import "../styles/UpdateVote.css"
+import { getAuth } from "firebase/auth";
 
 export default function UpdateVote({ sneakerId, sneaker, setSneakers }) {
-  // const [thumbsUp, setThumbsUp] = useState(0);
-  // const [thumbsDown, setThumbsDown] = useState(0);
-
-  // useEffect(() => {
-  //   // Fetch the current votes when the component mounts
-  //   fetchVotes();
-  // }, [sneakerId]);
-
-  // const fetchVotes = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://lacetalk-iv.web.app/sneaker/${sneakerId}`
-  //     );
-  //     const data = await response.json();
-  //     setThumbsUp(data.thumbsUp);
-  //     setThumbsDown(data.thumbsDown);
-  //   } catch (error) {
-  //     console.error("Error fetching votes:", error);
-  //   }
-  // };
-
-
+  const auth = getAuth();
 
   const voteUp = async () => {
+    if (!auth.currentUser) {
+      alert("Please log in to vote");
+      return;
+    }
     try {
       const response = await fetch(
         `https://lacetalk-iv.web.app/sneakers/${sneakerId}/voteUp`,
         { method: "POST" }
       );
       const data = await response.json();
-      setSneakers(data)
+      setSneakers(data);
       // setThumbsUp(data.value.thumbsUp); // Updated line
     } catch (error) {
       console.error("Error voting up:", error);
@@ -42,20 +26,22 @@ export default function UpdateVote({ sneakerId, sneaker, setSneakers }) {
   };
 
   const voteDown = async () => {
+    if (!auth.currentUser) {
+      alert("Please log in to vote");
+      return;
+    }
     try {
       const response = await fetch(
         `https://lacetalk-iv.web.app/sneakers/${sneakerId}/voteDown`,
         { method: "POST" }
       );
       const data = await response.json();
-      setSneakers(data)
+      setSneakers(data);
       // setThumbsDown(data.value.thumbsDown); // Updated line
     } catch (error) {
       console.error("Error voting down:", error);
     }
   };
-
-
 
   return (
     <div>
